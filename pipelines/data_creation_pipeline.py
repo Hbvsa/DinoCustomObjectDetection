8ee958d9cb831246e2b1d00d3a9338238045a19b
Hbvsa <61920247+Hbvsa@users.noreply.github.com>
2024-06-24 17:11:06 +0100

    Update data_creation_pipeline.py

--- pipelines/data_creation_pipeline.py
+++ pipelines/data_creation_pipeline.py
@@ -1,6 +1,6 @@
 from zenml import pipeline
 from steps.data_creation import data_creation
 
-@pipeline(enable_cache=True)
+@pipeline(enable_cache=False)
 def data_creation_pipeline():
     data_creation()
